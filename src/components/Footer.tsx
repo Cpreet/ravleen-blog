a1e3fa6f200585d7526fc4e89b4e13339ffb5d45
cpreet <cpreet.chawla@gmail.com>
2025-05-11 09:11:22 +0530

    feat: added footer component with changes to output.css

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer className="flex items-center bg-stone-400 w-screen h-40">
+            <div className="flex justify-center mx-auto text-center gap-8">
+                <a href="instagram.com/ravleen.ink" className="text-gray-700 hover:text-gray-900">
+                    <p className="text-sm">Instagram</p>
+                </a>
+                <a href="instagram.com/ravleen.ink" className="text-gray-700 hover:text-gray-900">
+                    <p className="text-sm">LinkedIn</p>
+                </a>
+                <a href="instagram.com/ravleen.ink" className="text-gray-700 hover:text-gray-900">
+                    <p className="text-sm">Twitter</p>
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
